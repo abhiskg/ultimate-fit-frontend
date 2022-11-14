a8a4f2d0c90c477d9a5aa11d307f4b2dbc96bf08
abhishek <abhiskg20@gmail.com>
2022-11-14 23:36:56 +0600

    Service Card loaded added

--- src/pages/home/Main.tsx
+++ src/pages/home/Main.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import ServiceMiniCard from "../../components/cards/ServiceMiniCard";
 import PingLoader from "../../components/loaders/PingLoader";
+import ServiceCardLoader from "../../components/loaders/ServiceCardLoader";
 import useDocTitle from "../../hooks/useDocTitle";
 import { useHomePageServicesData } from "../../hooks/useServicesData";
 import { ServiceDataTypes } from "../../types/ServiceTypes";
@@ -13,17 +14,20 @@ const Main = () => {
   const { data, isLoading } = useHomePageServicesData(0, 3);
   const services = data?.data.data as ServiceDataTypes[];
 
-  if (isLoading) {
-    return <PingLoader />;
-  }
-
   return (
     <div className="custom-width mx-auto">
       <div>
         <Hero />
       </div>
+      {isLoading && (
+        <div className="grid w-full gap-3 sm:grid-cols-2 lg:grid-cols-3">
+          <ServiceCardLoader />
+          <ServiceCardLoader />
+          <ServiceCardLoader />
+        </div>
+      )}
       <div className="grid w-full gap-3 sm:grid-cols-2 lg:grid-cols-3">
-        {services.length > 0 &&
+        {services &&
           services.map((service) => (
             <ServiceMiniCard key={service._id} service={service} />
           ))}
